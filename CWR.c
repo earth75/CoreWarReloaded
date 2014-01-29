a91e194900f037bc2958e58f524bec150a635f51
earth75 <alexis.tetar@gmail.com>
2014-01-29 19:34:01 +0100

    no inline comments in c, lol
    yeah i decided to code in C ansi, silly me. Why? Because i'm HARDCORE

--- CWR.c
+++ CWR.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 int main(int argc, char* argv[]) {
 
-    SDL_Window *window;                    // Declare a pointer
+    SDL_Window *window;
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    SDL_Init(SDL_INIT_VIDEO);
 
-    // Create an application window with the following settings:
+
     window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
+        "CodeWarsReloaded",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        1280,
+        800,
+        0
     );
 
-    // Check that the window was successfully made
+
     if (window == NULL) {
-        // In the event that the window could not be made...
         printf("Could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
-    // The window is open: enter program loop (see SDL_PollEvent)
 
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+    SDL_Delay(3000);
 
-    // Close and destroy the window
     SDL_DestroyWindow(window);
 
-    // Clean up
     SDL_Quit();
     return 0;
 }
