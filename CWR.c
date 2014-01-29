b862768a5b7f9464150982f7a37c52cb35ec6bc1
earth75 <alexis.tetar@gmail.com>
2014-01-29 22:48:08 +0100

    rederer thing + finished renaming

--- CWR.c
+++ CWR.c
@@ -9,12 +9,16 @@
 int main(int argc, char* argv[]) {
 
     SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *bitmapSurface = NULL;
+    SDL_Texture *bitmapTex = NULL;
+
 
     SDL_Init(SDL_INIT_VIDEO);
 
 
     window = SDL_CreateWindow(
-        "CodeWarsReloaded",
+        "CoreWarReloaded",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         MAINWIN_WIDTH,
@@ -28,8 +32,29 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bitmapSurface = SDL_LoadBMP("test.bmp");
+
+    bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
+    SDL_FreeSurface(bitmapSurface);
+
+    while (1) {
+        SDL_Event e;
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                break;
+            }
+        }
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    }
 
 
+    SDL_DestroyTexture(bitmapTex);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
